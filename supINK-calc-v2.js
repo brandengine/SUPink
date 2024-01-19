3bfbdeae54d15e25ef2618cb8085140b61839657
brandengine <justin@brandengine.co>
2024-01-19 11:28:05 -0500

    Update supINK-calc-v2.js
    Added all em calls

--- supINK-calc-v2.js
+++ supINK-calc-v2.js
@@ -48,7 +48,25 @@ var RUSHseven = document.getElementById("7-9-DAY-TURN");
 var RUSHzero = document.getElementById("No-Rush");
 
 //EM Embroidery START
+var emneckTAG = document.getElementById("Embroidery-Neck-Tag");
+var emprintTAG = document.getElementById("Embroidery-Tag-Print-Removal");
+var emcutTAG = document.getElementById("Embroidery-Tag-Cut-Removal");
+var emfoldOPT = document.getElementById("Embroidery-Folding");
+var emfoldBAG = document.getElementById("Embroidery-Fold-Bag");
+var embandTAG = document.getElementById("Embroidery-Roll-Band-Tag");
+var emwLABEL = document.getElementById("Embroidery-Woven-Label");
+var emhTag = document.getElementById("Embroidery-Hang-Tag");
+var emsideSEW = document.getElementById("Embroidery-4-sided-sew");
+var emseamRIP = document.getElementById("Embroidery-Seam-Rip-Restitch");
+var emseamCAPS = document.getElementById("Embroidery-SEAM-RIP-CAPS");
+var emUPCstick = document.getElementById("Embroidery-UPC-sticker");
 
+//shipping OPTIONS
+var emRUSHone = document.getElementById("Embroidery-1-2-DAY-TURN");
+var emRUSHthree = document.getElementById("Embroidery-3-4-DAY-TURN");
+var emRUSHfive = document.getElementById("Embroidery-5-6-DAY-TURN");
+var emRUSHseven = document.getElementById("Embroidery-7-9-DAY-TURN");
+var emRUSHzero = document.getElementById("Embroidery-No-Rush");
 
 //HEAT PRESS START
 var hpSMALL = document.getElementById("Small-Print");
@@ -106,6 +124,28 @@ document.getElementById("No-Rush").addEventListener("change", update);
 
 //Embroidery OPTIONS
 document.getElementById("Embroidery-QTY").addEventListener("change", update);
+//tagging OPTIONS
+document.getElementById("Embroidery-Neck-Tag").addEventListener("change", update);
+document.getElementById("Embroidery-Tag-Print-Removal").addEventListener("change", update);
+document.getElementById("Embroidery-Tag-Cut-Removal").addEventListener("change", update);
+//folding OPTIONS
+document.getElementById("Embroidery-Folding").addEventListener("change", update);
+document.getElementById("Embroidery-Fold-Bag").addEventListener("change", update);
+document.getElementById("Embroidery-Roll-Band-Tag").addEventListener("change", update);
+//sewing OPTIONS
+document.getElementById("Embroidery-Woven-Label").addEventListener("change", update);
+document.getElementById("Embroidery-Hang-Tag").addEventListener("change", update);
+document.getElementById("Embroidery-4-sided-sew").addEventListener("change", update);
+document.getElementById("Embroidery-Seam-Rip-Restitch").addEventListener("change", update);
+document.getElementById("Embroidery-SEAM-RIP-CAPS").addEventListener("change", update);
+//sticker OPTIONS
+document.getElementById("Embroidery-UPC-sticker").addEventListener("change", update);
+//shipping OPTIONS
+document.getElementById("Embroidery-1-2-DAY-TURN").addEventListener("change", update);
+document.getElementById("Embroidery-3-4-DAY-TURN").addEventListener("change", update);
+document.getElementById("Embroidery-5-6-DAY-TURN").addEventListener("change", update);
+document.getElementById("Embroidery-7-9-DAY-TURN").addEventListener("change", update);
+document.getElementById("Embroidery-No-Rush").addEventListener("change", update);
 
 //HEAT PRESS OPTIONS
 document.getElementById("Transfer-QTY").addEventListener("change", update);
@@ -201,6 +241,13 @@ RUSHzero = document.getElementById("No-Rush");
 //Embroidery OPTIONS
 emQTY = document.getElementById("Embroidery-QTY").value;
 
+//shipping OPTIONS
+emRUSHone = document.getElementById("Embroidery-1-2-DAY-TURN");
+emRUSHthree = document.getElementById("Embroidery-3-4-DAY-TURN");
+emRUSHfive = document.getElementById("Embroidery-5-6-DAY-TURN");
+emRUSHseven = document.getElementById("Embroidery-7-9-DAY-TURN");
+emRUSHzero = document.getElementById("Embroidery-No-Rush");
+
 //HEAT PRESS OPTIONS
 hpQTY = document.getElementById("Transfer-QTY").value;
 hpSMALL = document.getElementById("Small-Print");
@@ -285,13 +332,70 @@ result = ((Number(colorAmount) * Number(productQTY)) + productPRICE) * printLOCs
 
   displayResult.textContent = result.toFixed(2);
 
-  //Embroidery CALC
+//Embroidery CALC
   emResult = productPRICE + Number(emQTY);
+  
+//tag CALC
+  if (emneckTAG.checked === true) {
+    emResult = Number(emResult) + Number(1.60);
+  }
+  if (emprintTAG.checked === true) {
+    emResult = Number(emResult) + Number(1.80);
+  }
+  if (emcutTAG.checked === true) {
+    emResult = Number(emResult) + Number(0.30);
+  }
+//fold CALC
+  if (emfoldOPT.checked === true) {
+    emResult = Number(emResult) + Number(0.40);
+  }
+  if (emfoldBAG.checked === true) {
+    emResult = Number(emResult) + Number(0.80);
+  }
+  if (embandTAG.checked === true) {
+    emResult = Number(emResult) + Number(0.70);
+  }
+//sewing CALC
+  if (emwLABEL.checked === true) {
+    emResult = Number(emResult) + Number(1.00);
+  }
+  if (emhTag.checked === true) {
+    emResult = Number(emResult) + Number(0.25);
+  }
+  if (emsideSEW.checked === true) {
+    emResult = Number(emResult) + Number(3.50);
+  }
+  if (emseamRIP.checked === true) {
+    emResult = Number(emResult) + Number(2);
+  }
+  if (emseamCAPS.checked === true) {
+    emResult = Number(emResult) + Number(2.50);
+  }
+//sticker CALC
+  if (emUPCstick.checked === true) {
+    emResult = Number(emResult) + Number(0.15);
+  }
+//shipping CALC
+  if (emRUSHone.checked === true) {
+    emResult = Number(emResult) * Number(2);
+  }
+  if (emRUSHthree.checked === true) {
+    emResult = Number(emResult) * Number(1.5);
+  }
+   if (emRUSHfive.checked === true) {
+    emResult = Number(emResult) * Number(1.3);
+  }
+   if (emRUSHseven.checked === true) {
+    emResult = Number(emResult) * Number(1.2);
+  }
+    if (emRUSHzero.checked === true) {
+    emResult = Number(emResult) * Number(1);
+  }
 
   emDisplayResult.textContent = emResult.toFixed(2);
 
 
-  //HEAT PRESS CALC
+//HEAT PRESS CALC
   hpSize = Number(7.65);
   if (hpLCHEST.checked === true) {
     hpSize = Number(8.10);
@@ -310,7 +414,8 @@ result = ((Number(colorAmount) * Number(productQTY)) + productPRICE) * printLOCs
   }
 
   hpResult = productPRICE + (Number(hpQTY) * Number(hpSize));
-   //shipping CALC
+
+//shipping CALC
     if (hpRUSHTHREE.checked === true) {
     hpResult = Number(hpResult) * Number(1.8);
   }
